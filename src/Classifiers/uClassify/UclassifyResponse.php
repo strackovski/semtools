0ce67472adf898a1c73b9fdcf9fe9265a598fa15
strackovski <vladimir.strackovski@gmail.com>
2014-08-14 22:43:19 +0000

    Implement classification retrieval as array.

--- src/Classifiers/uClassify/UclassifyResponse.php
+++ src/Classifiers/uClassify/UclassifyResponse.php
@@ -22,5 +22,28 @@ use nv\semtools;
  */
 class UclassifyResponse extends semtools\Common\ApiResponseAbstract
 {
-    // @todo Encapsulate response data: parse XML
+    /**
+     * Get classification data as PHP array
+     *
+     * @return array
+     * @throws \nv\semtools\Exception\ServiceReaderException
+     */
+    public function getClassification()
+    {
+        $xml = simplexml_load_string($this->response);
+        $result = array();
+
+        if ($xml instanceof \SimpleXMLElement) {
+            if ((string) $xml->status->attributes()->success == 'true') {
+                foreach ($xml->readCalls->classify as $classify) {
+                    foreach ($classify->classification->class as $class) {
+                        $result[strtolower($class->attributes()->className)] = (float) $class->attributes()->p;
+                    }
+                }
+                return $result;
+            }
+            throw new semtools\Exception\ServiceReaderException($xml->status->attributes()->statusCode);
+        }
+        throw new semtools\Exception\ServiceReaderException('Failed parsing XML response.');
+    }
 }
